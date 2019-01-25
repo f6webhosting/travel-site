0d8d5d9cf96920e2340f68a4089123baaa2372ad
Khalil <f6webhosting@gmail.com>
2019-01-24 19:47:41 -0600

    went back to first watch--started over--

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,9 @@ gulp.task('html', function(){
     console.log("Imagine somehing useful being done to your HTML here.")
 });
 
-gulp.task('styles', function(){
-    console.log("Imagine somehing SASS or PostCSS running here.")
-});
-
 gulp.task('watch', function(){
 
     watch('./app/index.html', function(){
         gulp.start('html');
-    });
-
-    watch('./app/assets/styles/**/*.css' function(){
-        gulp.start('styles')
     });
 });
